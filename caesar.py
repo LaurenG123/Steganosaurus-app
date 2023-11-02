c6abee38cc27f5affec71a6af30bfd29ebeed5dc
TimothyGorst <tim.gorst66@gmail.com>
2023-11-02 11:57:15 +0000

    Added error-checking

--- caesar.py
+++ caesar.py
@@ -5,14 +5,20 @@ class Caesar:
         
     # takes message and returns encrypted message using shift defined above
     def encrypt(self,msg):
-        alphabet = string.ascii_lowercase
-        shifted_alphabet = alphabet[int(self.shift):] + alphabet[:int(self.shift)]
-        table = str.maketrans(alphabet, shifted_alphabet)
-        return msg.translate(table)
+        if msg.isalpha() and int(self.shift) >= 0 and int(self.shift) < 26:
+            alphabet = string.ascii_lowercase
+            shifted_alphabet = alphabet[int(self.shift):] + alphabet[:int(self.shift)]
+            table = str.maketrans(alphabet, shifted_alphabet)
+            return msg.translate(table)
+        else:
+            return "invalid input"
 
     # takes encrypted message and returns plaintext using shift defined above
     def decrypt(self,msg):
-        alphabet = string.ascii_lowercase
-        shifted_alphabet = alphabet[26-int(self.shift):] + alphabet[:26-int(self.shift)]
-        table = str.maketrans(alphabet, shifted_alphabet)
-        return msg.translate(table)
+        if msg.isalpha() and int(self.shift) >= 0 and int(self.shift) < 26:
+            alphabet = string.ascii_lowercase
+            shifted_alphabet = alphabet[26-int(self.shift):] + alphabet[:26-int(self.shift)]
+            table = str.maketrans(alphabet, shifted_alphabet)
+            return msg.translate(table)
+        else:
+            return "invalid input"
